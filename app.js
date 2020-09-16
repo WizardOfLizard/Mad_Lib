7f38747ed2474678db2d49f0d8234f5cff027a2e
Daniel Hensler <henslerdl@gmail.com>
2020-09-16 08:51:36 -0600

    Working mad lib prompts

---
+++ app.js
@@ -0,0 +1,34 @@
+let adjective1El = document.getElementById("adjective1")
+let noun1El = document.getElementById("noun1")
+let pluralnoun1El = document.getElementById("pluralnoun1")
+let femaleEl = document.getElementById("female")
+let adjective2El = document.getElementById("adjective2")
+let clothingEl = document.getElementById("clothing")
+let noun2El = document.getElementById("noun2")
+let cityEl = document.getElementById("city")
+let pluralnoun2El = document.getElementById("pluralnoun2")
+let adjective3El = document.getElementById("adjective3")
+
+console.log = ("Working")
+
+let adjective1 = prompt("Provide an adjective.")
+let noun1 = prompt("Provide a noun.")
+let pluralnoun1 = prompt("Provide a plural noun.")
+let female = prompt("Provide a female name.")
+let adjective2 = prompt("Provide an adjective.")
+let clothing = prompt("Provide an article of clothing.")
+let noun2 = prompt("Provide a noun.")
+let city = prompt("Provide a city name.")
+let pluralnoun2 = prompt("Provide a plural noun.")
+let adjective3 = prompt("Provide an adjective.")
+
+adjective1El.innerHTML = adjective1
+noun1El.innerHTML = noun1
+pluralnoun1El.innerHTML = document.pluralnoun1
+femaleEl.innerHTML = female
+adjective2El.innerHTML = adjective2
+clothingEl.innerHTML = clothing
+noun2El.innerHTML = noun2
+cityEl.innerHTML = city
+pluralnoun2El.innerHTML = pluralnoun2
+adjective3El.innerHTML = adjective3
